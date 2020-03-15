4f58fb40be780c19ac8d558b34f52fc8540af48e
dimasdom <54259739+dimasdom@users.noreply.github.com>
2020-03-15 20:13:03 +0200

    Update api.ts

--- src/redux/api/api.ts
+++ src/redux/api/api.ts
@@ -1,5 +1,5 @@
 import  axios from 'axios'
 const API_KEY:string = "bf1c48646ae741489de3fdc2e6f28153";
 export const getNewsAPI = async (category:string) =>{
-    return axios.get(`http://newsapi.org/v2/top-headlines?country=ua${category ? "&category="+category : ""}&apiKey=${API_KEY}`).then(response => {return response.data})
+    return axios.get(`https://newsapi.org/v2/top-headlines?country=ua${category ? "&category="+category : ""}&apiKey=${API_KEY}`).then(response => {return response.data})
 }
